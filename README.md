0fff6c7952f57bb5afa18ac8794ac00191935898
Herve Senot <herve.senot@groundtruth.com.au>
2014-01-27 13:49:56 +1100

    Initial working version

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ worklife
 ========
 
 Code for the work/life balance installation.
+
+* Requirements:
+- PHP5
+- cURL
+sudo apt-get install php5-curl
+
+* Modify the settings files to fit your Twitter dev user account / app.
+
+* Ignoring the changes to the settings file can be obtained using:
+http://gitready.com/intermediate/2009/02/18/temporarily-ignoring-files.html
--- get_tweet.php
+++ get_tweet.php
@@ -3,14 +3,29 @@
 	require_once('twitter-api-php/TwitterAPIExchange.php');
 	require_once('settings.php');
 
+	// Extracting from 'about' parameter
+	if (isset($_REQUEST['about']))
+	{
+		$p_word = $_REQUEST['about'];
+	}
+
+	// This is in case the parameter is not accessible
+	if (!$p_word)	
+	{
+		$p_word = 'work';
+	}
+
 	/** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
 	$url = 'https://api.twitter.com/1.1/search/tweets.json';
 	$requestMethod = 'GET';
 
-	$getfield = '?q=%23work';
+	$getfield = '?q=%23'.$p_word.'&result_type=recent&count=12';
 	$twitter = new TwitterAPIExchange($settings);
+
+	// Interpreting the result as simple JSON
+	header("Content-Type: application/json");
 	echo $twitter->setGetfield($getfield)
 	             ->buildOauth($url, $requestMethod)
-	             ->performRequest();     
+	             ->performRequest();
 
 ?>
